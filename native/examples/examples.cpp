d9db8f9bb036a5fd460f999bc80d9280f25226e4
hongyu <h.tsai@hotmail.com>
2022-04-18 10:02:30 -0400

    examples.cpp

--- native/examples/examples.cpp
+++ native/examples/examples.cpp
@@ -6,100 +6,158 @@
 using namespace std;
 using namespace seal;
 
+Ciphertext compute(
+    Plaintext x_plain, Ciphertext x1_encrypted, Plaintext plain_coeff3, Plaintext plain_coeff1, Plaintext plain_coeff0,
+    double scale, const SEALContext &context, const RelinKeys &relin_keys, const Encryptor &encryptor,
+    const Evaluator &evaluator, CKKSEncoder &encoder, const vector<double> &input)
+{ /*
+We create plaintexts for PI, 0.4, and 1 using an overload of CKKSEncoder::encode
+that encodes the given floating-point value to every slot in the vector.
+*/
+
+    /*
+    To compute x^3 we first compute x^2 and relinearize. However, the scale has
+    now grown to 2^80.
+    */
+    Ciphertext x3_encrypted;
+    evaluator.square(x1_encrypted, x3_encrypted);
+    evaluator.relinearize_inplace(x3_encrypted, relin_keys);
+
+    /*
+    Now rescale; in addition to a modulus switch, the scale is reduced down by
+    a factor equal to the prime that was switched away (40-bit prime). Hence, the
+    new scale should be close to 2^40. Note, however, that the scale is not equal
+    to 2^40: this is because the 40-bit prime is only close to 2^40.
+    */
+    evaluator.rescale_to_next_inplace(x3_encrypted);
+
+    /*
+    Now x3_encrypted is at a different level than x1_encrypted, which prevents us
+    from multiplying them to compute x^3. We could simply switch x1_encrypted to
+    the next parameters in the modulus switching chain. However, since we still
+    need to multiply the x^3 term with PI (plain_coeff3), we instead compute PI*x
+    first and multiply that with x^2 to obtain PI*x^3. To this end, we compute
+    PI*x and rescale it back from scale 2^80 to something close to 2^40.
+    */
+    Ciphertext x1_encrypted_coeff3;
+    evaluator.multiply_plain(x1_encrypted, plain_coeff3, x1_encrypted_coeff3);
+    evaluator.rescale_to_next_inplace(x1_encrypted_coeff3);
+
+    /*
+    Since x3_encrypted and x1_encrypted_coeff3 have the same exact scale and use
+    the same encryption parameters, we can multiply them together. We write the
+    result to x3_encrypted, relinearize, and rescale. Note that again the scale
+    is something close to 2^40, but not exactly 2^40 due to yet another scaling
+    by a prime. We are down to the last level in the modulus switching chain.
+    */
+    evaluator.multiply_inplace(x3_encrypted, x1_encrypted_coeff3);
+    evaluator.relinearize_inplace(x3_encrypted, relin_keys);
+    evaluator.rescale_to_next_inplace(x3_encrypted);
+
+    /*
+    Next we compute the degree one term. All this requires is one multiply_plain
+    with plain_coeff1. We overwrite x1_encrypted with the result.
+    */
+    evaluator.multiply_plain_inplace(x1_encrypted, plain_coeff1);
+    evaluator.rescale_to_next_inplace(x1_encrypted);
+
+    x3_encrypted.scale() = pow(2.0, 40);
+    x1_encrypted.scale() = pow(2.0, 40);
+
+    /*
+    We still have a problem with mismatching encryption parameters. This is easy
+    to fix by using traditional modulus switching (no rescaling). CKKS supports
+    modulus switching just like the BFV scheme, allowing us to switch away parts
+    of the coefficient modulus when it is simply not needed.
+    */
+    parms_id_type last_parms_id = x3_encrypted.parms_id();
+    evaluator.mod_switch_to_inplace(x1_encrypted, last_parms_id);
+    evaluator.mod_switch_to_inplace(plain_coeff0, last_parms_id);
+
+    Ciphertext encrypted_result;
+    evaluator.add(x3_encrypted, x1_encrypted, encrypted_result);
+    evaluator.add_plain_inplace(encrypted_result, plain_coeff0);
+    return encrypted_result;
+}
+
 int main()
 {
-    cout << "Microsoft SEAL version: " << SEAL_VERSION << endl;
-    while (true)
+    EncryptionParameters parms(scheme_type::ckks);
+
+    size_t poly_modulus_degree = 8192;
+    parms.set_poly_modulus_degree(poly_modulus_degree);
+    parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, { 60, 40, 40, 60 }));
+
+    double scale = pow(2.0, 40);
+
+    SEALContext context(parms);
+    print_parameters(context);
+    cout << endl;
+
+    KeyGenerator keygen(context);
+    auto secret_key = keygen.secret_key();
+    PublicKey public_key;
+    keygen.create_public_key(public_key);
+    RelinKeys relin_keys;
+    keygen.create_relin_keys(relin_keys);
+    GaloisKeys gal_keys;
+    keygen.create_galois_keys(gal_keys);
+    Encryptor encryptor(context, public_key);
+    Evaluator evaluator(context);
+    Decryptor decryptor(context, secret_key);
+
+    CKKSEncoder encoder(context);
+    size_t slot_count = encoder.slot_count();
+    cout << "Number of slots: " << slot_count << endl;
+
+    vector<double> input;
+    input.reserve(slot_count);
+    double curr_point = 0;
+    double step_size = 1.0 / (static_cast<double>(slot_count) - 1);
+    for (size_t i = 0; i < slot_count; i++)
     {
-        cout << "+---------------------------------------------------------+" << endl;
-        cout << "| The following examples should be executed while reading |" << endl;
-        cout << "| comments in associated files in native/examples/.       |" << endl;
-        cout << "+---------------------------------------------------------+" << endl;
-        cout << "| Examples                   | Source Files               |" << endl;
-        cout << "+----------------------------+----------------------------+" << endl;
-        cout << "| 1. BFV Basics              | 1_bfv_basics.cpp           |" << endl;
-        cout << "| 2. Encoders                | 2_encoders.cpp             |" << endl;
-        cout << "| 3. Levels                  | 3_levels.cpp               |" << endl;
-        cout << "| 4. BGV Basics              | 4_bgv_basics.cpp           |" << endl;
-        cout << "| 5. CKKS Basics             | 5_ckks_basics.cpp          |" << endl;
-        cout << "| 6. Rotation                | 6_rotation.cpp             |" << endl;
-        cout << "| 7. Serialization           | 7_serialization.cpp        |" << endl;
-        cout << "| 8. Performance Test        | 8_performance.cpp          |" << endl;
-        cout << "+----------------------------+----------------------------+" << endl;
-
-        /*
-        Print how much memory we have allocated from the current memory pool.
-        By default the memory pool will be a static global pool and the
-        MemoryManager class can be used to change it. Most users should have
-        little or no reason to touch the memory allocation system.
-        */
-        size_t megabytes = MemoryManager::GetPool().alloc_byte_count() >> 20;
-        cout << "[" << setw(7) << right << megabytes << " MB] "
-             << "Total allocation from the memory pool" << endl;
-
-        int selection = 0;
-        bool valid = true;
-        do
-        {
-            cout << endl << "> Run example (1 ~ 8) or exit (0): ";
-            if (!(cin >> selection))
-            {
-                valid = false;
-            }
-            else if (selection < 0 || selection > 8)
-            {
-                valid = false;
-            }
-            else
-            {
-                valid = true;
-            }
-            if (!valid)
-            {
-                cout << "  [Beep~~] valid option: type 0 ~ 8" << endl;
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            }
-        } while (!valid);
-
-        switch (selection)
-        {
-        case 1:
-            example_bfv_basics();
-            break;
-
-        case 2:
-            example_encoders();
-            break;
-
-        case 3:
-            example_levels();
-            break;
-
-        case 4:
-            example_bgv_basics();
-            break;
-
-        case 5:
-            example_ckks_basics();
-            break;
-
-        case 6:
-            example_rotation();
-            break;
-
-        case 7:
-            example_serialization();
-            break;
-
-        case 8:
-            example_performance_test();
-            break;
-
-        case 0:
-            return 0;
-        }
+        input.push_back(curr_point);
+        curr_point += step_size;
     }
+    cout << "Input vector: " << endl;
+    print_vector(input, 3, 7);
 
-    return 0;
+    cout << "Evaluating polynomial PI*x^3 + 0.4x + 1 ..." << endl;
+    Plaintext plain_coeff3, plain_coeff1, plain_coeff0;
+    encoder.encode(3.14159265, scale, plain_coeff3);
+    encoder.encode(0.4, scale, plain_coeff1);
+    encoder.encode(1.0, scale, plain_coeff0);
+
+    Plaintext x_plain;
+    encoder.encode(input, scale, x_plain);
+    Ciphertext x1_encrypted;
+    encryptor.encrypt(x_plain, x1_encrypted);
+
+    Ciphertext encrypted_result = compute(
+        x_plain, x1_encrypted, plain_coeff3, plain_coeff1, plain_coeff0, scale, context, relin_keys, encryptor,
+        evaluator, encoder, input);
+
+    /*
+    First print the true result.
+    */
+    Plaintext plain_result;
+    print_line(__LINE__);
+    cout << "Decrypt and decode PI*x^3 + 0.4x + 1." << endl;
+    cout << "    + Expected result:" << endl;
+    vector<double> true_result;
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        double x = input[i];
+        true_result.push_back((3.14159265 * x * x + 0.4) * x + 1);
+    }
+    print_vector(true_result, 3, 7);
+
+    /*
+    Decrypt, decode, and print the result.
+    */
+    decryptor.decrypt(encrypted_result, plain_result);
+    vector<double> result;
+    encoder.decode(plain_result, result);
+    cout << "    + Computed result ...... Correct." << endl;
+    print_vector(result, 3, 7);
 }
